df54a492b35109863644d07a4fecfed507973304
Anshul-Sangrame <anshul.sangrame@gmail.com>
2023-10-27 16:44:25 +0530

    added tokens

--- src/auxilary.hh
+++ src/auxilary.hh
@@ -1,12 +1,35 @@
 #ifndef AUXILARY_HH
 #define AUXILARY_HH
-#include "driver.hh"
-#include "parser.tab.hh"
+#include <string>
+#include <vector>
 using namespace std;
 
-namespace helper {
-    // define helping functions here
-    
+namespace helper
+{
+    // define helping functions and classes here
+
+    // splits the string given delimiter
+    vector<string> split(string s, string del);
+
+    class date
+    {
+    public:
+        int year;
+        int month;
+        int day;
+        date();
+        date(string str);
+    };
+
+    class time
+    {
+    public:
+        int hour;
+        int min;
+        int sec;
+        time();
+        time(string str);
+    };
 }
 
 #endif
