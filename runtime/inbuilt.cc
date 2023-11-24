044682525f53eb0e963fcd09e92e10922041bc2e
procodervarun <thecodervarun@gmail.com>
2023-11-24 14:27:08 +0530

    sum func for table

--- runtime/inbuilt.cc
+++ runtime/inbuilt.cc
@@ -289,3 +289,58 @@ any DISP(const any &a,const pos &p)
     st.outfunc();
     return any();
 }
+
+any ADD(const any &a, const any &b, const pos &p)
+{
+    st.infunc(p);
+    if (a.type == "int" && b.type == "int")
+    {
+        int *ptr1 = (int *)a.data;
+        int *ptr2 = (int *)b.data;
+        st.outfunc();
+        return any(new int(*ptr1 + *ptr2), "int");
+    }
+    if (
+        (a.type == "double" && b.type == "int") ||
+        (a.type == "int" && b.type == "double") ||
+        (a.type == "double" && b.type == "double"))
+    {
+        double *ptr1 = (double *)a.data;
+        double *ptr2 = (double *)b.data;
+        st.outfunc();
+        return any(new double(*ptr1 + *ptr2), "double");
+    }
+    throw bi_err("ADD", a, b);
+}
+
+// any sum(any &tab1, const pos &p)
+// {
+//     st.infunc(p);
+//     if(tab1.type == "cell")
+//     {
+//         double *ptr1 = (double *)tab1.data;
+//         st.outfunc();
+//         return any(new double(*ptr1), "double");
+//     }
+//     if(tab1.type == "table")
+//     {
+//         double total = 0.0;
+//         for (const auto &entry : tab1.data.tb){
+//             double value = (double *)entry.second.val;
+//             total += value;
+//         }
+//         st.outfunc();
+//         return any(new double(total), "double");
+//     }
+//     st.outfunc();
+//     throw uni_error("SUM",tab1);
+// }
+any minimum(any &tab1);
+any maximum(any &tab1);
+any average(any &tab1);
+any product(any &tab1);
+any count(any &tab1);
+any ceiling(any &tab1);
+any floor(any &tab1);
+any modulus(any &tab1, any &N);
+any power(any &tab1, any &N);
